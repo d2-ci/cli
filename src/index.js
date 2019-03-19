529642ed896811790b0be34f9ac84b281124d1e1
Austin McGee <947888+amcgee@users.noreply.github.com>
2019-03-19 22:41:46 +0000

    4605559 feat: revamp cluster management

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const { namespace } = require('@dhis2/cli-helpers-engine')
+
+const command = namespace('d2', {
+    desc: 'DHIS2 CLI',
+    builder: yargs => {
+        yargs.commandDir('commands')
+        yargs.command(require('@dhis2/cli-create'))
+        yargs.command(require('@dhis2/cli-app'))
+        yargs.command(require('@dhis2/cli-cluster'))
+        // yargs.command(require('@dhis2/cli-packages'))
+        yargs.command(require('@dhis2/cli-style'))
+        yargs.command(require('@dhis2/cli-utils'))
+    },
+})
+
+module.exports = command
